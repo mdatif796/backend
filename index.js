297dd9d9ee8570bf9038d6f445c6f2ecda65a8fb
mdatif796 <mdatif796@gmail.com>
2023-09-27 01:00:43 +0530

    added message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello backend" });
+  res.status(200).json({ message: "Hello Atif" });
 });
 
 app.listen(3000);
